8c5b8efc2a5af6da5f474fdb82f11f0c0cf370af
BHADDRESH1 <bhaddreshamudala@gmail.com>
2025-11-11 20:06:31 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // this makes Next.js generate static files
-  images: { unoptimized: true }, // needed if you use <Image>
-};
-
-module.exports = nextConfig;
-
